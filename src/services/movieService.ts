0ad9dc2ccddaabd8f1a74aa05c0082563456b469
Alyonas999 <oranchuka@gmail.com>
2025-08-02 19:00:23 +0300

    movie

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,20 +1,13 @@
-import axios from "axios";
-import { type Movie } from "../types/movie";
+import axios from 'axios';
+import type { Movie } from '../types/movie';
 
-
-const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
-const options = {
-  method: 'GET',
+const tmdbApi = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjgxYWY4ZGRjOGU2NTg0M2NkZTU5MGE5YmE2OWMwNSIsIm5iZiI6MTc1NDEyNjg0Ni4wNDksInN1YiI6IjY4OGRkOWZlNzQzNTk0YmIxOGYzZGMyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WM_vBCDn2Lya1iHbvDx5ajawREIRwfvJE2gWVAat4DE'
-  }
-};
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjgxYWY4ZGRjOGU2NTg0M2NkZTU5MGE5YmE2OWMwNSIsIm5iZiI6MTc1NDEyNjg0Ni4wNDksInN1YiI6IjY4OGRkOWZlNzQzNTk0YmIxOGYzZGMyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WM_vBCDn2Lya1iHbvDx5ajawREIRwfvJE2gWVAat4DE',
+  },
+});
 
 interface FetchMoviesResponse {
   page: number;
@@ -23,9 +16,15 @@ interface FetchMoviesResponse {
   total_results: number;
 }
 
+
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
-  const response = await tmdbApi.get<FetchMoviesResponse>("search/movie", {
-    params: { query },
+  const response = await tmdbApi.get<FetchMoviesResponse>('search/movie', {
+    params: {
+      query,
+      include_adult: false,
+      language: 'en',
+      page: 1,
+    },
   });
 
   return response.data.results;
