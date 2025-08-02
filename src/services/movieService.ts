adfb8634d5ad03eaaeb13039bb933bd5fdd135f9
Alyonas999 <oranchuka@gmail.com>
2025-08-02 22:56:11 +0300

    end

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { Movie } from "../../types/movie";
+import type { Movie } from "../types/movie";
 
 const tmdbApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
